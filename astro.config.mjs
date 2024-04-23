fd28d8c36b407ba2df14fe91be9bc1240f632290
IsraelChidera <israelchidera54@gmail.com>
2024-04-23 13:42:39 +0100

    Added Google Analytics to blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
+import partytown from '@astrojs/partytown';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+  site: 'https://israelchidera.netlify.app/',
+  integrations: [react(), 
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      }
+    })
+  ]
+}); 
