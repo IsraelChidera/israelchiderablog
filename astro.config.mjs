ddd0659e80e42723688de347052d301d939e5f7e
IsraelChidera <israelchidera54@gmail.com>
2024-04-23 15:02:59 +0100

    sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import partytown from '@astrojs/partytown';
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://israelchidera.netlify.app/',
   integrations: [react(), 
+    sitemap(),
     tailwind(),
     partytown({
       config: {
