614cddfcfc4892474b9b65c3e6909da47b3a50e9
IsraelChidera <israelchidera54@gmail.com>
2024-05-27 22:24:51 +0100

    update

--- src/components/widgets/ProjectCards.tsx
+++ src/components/widgets/ProjectCards.tsx
@@ -13,11 +13,13 @@ const ProjectCards = () => {
             name: "FeedbackShare app",
             description: `FeedbackShare - A feedback management tool`,
             roles: ["Frontend developer", "Designer"],
+            link: "http://feedback-share.vercel.app/"
         },
         {
             name: "Lumixus Stduio",
             description: `Lumixus studio - A website for a web-design agency.`,
             roles: ["Frontend developer", "SEO manager", "Digital creative"],
+            link: "https://lumixus.studio/"
         },
         {
             name: "Pharmabolt",
@@ -25,22 +27,25 @@ const ProjectCards = () => {
             medication delivered at your door without delay or 
             hassle and book appointment with professionals`,
             roles: ["Frontend developer", "Designer", "Database management"],
-        },        
+            link: "https://pharmaboltapp.vercel.app/"
+        },
     ]
 
     return (
         <div className='mt-4 space-y-6 pb-20'>
             {
-                projects.map(({name, description, roles}) => (
+                projects.map(({ name, description, roles, link }) => (
                     <Card key={name} className='bg-[#242629] text-white '>
                         <CardHeader>
                             <CardTitle>{name}</CardTitle>
                             <CardDescription>{description}</CardDescription>
                         </CardHeader>
                         <CardFooter>
-                            <div className='flex text-sm items-center space-x-2'>
-                                <p>Link</p>
-                                <p>↗</p>
+                            <div>
+                                <a href={link} className="cursor-pointer flex text-sm items-center space-x-2">
+                                    <p>Link</p>
+                                    <p>↗</p>
+                                </a>
                             </div>
                         </CardFooter>
                     </Card>
